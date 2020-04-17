4679f3f77978c379f624247e747102715665b690
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-18 01:49:19 +0200

    Add blood

--- Assets/Scripts/WeaponDamage.cs
+++ Assets/Scripts/WeaponDamage.cs
@@ -6,11 +6,23 @@ public class WeaponDamage : MonoBehaviour
 {
     [Tooltip("Cantidad de daño")]
     public int damage;
+    public GameObject bloodAnim;
+    private GameObject hitPoint;
+
+    private void Start()
+    {
+        hitPoint = transform.Find("HitPoint").gameObject;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Enemy"))
         {
+            if (bloodAnim && hitPoint)
+            {
+                Destroy(Instantiate(bloodAnim, hitPoint.transform.position, hitPoint.transform.rotation), 0.5f);
+            }
+
             collision.gameObject
                 .GetComponent<HealthManager>()
                 .DamageCharacter(this.damage);
