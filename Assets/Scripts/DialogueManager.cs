54a7d3d7499922cd55f2f2ece3b6e6beb4beaa7b
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-19 01:51:26 +0200

    Add dialogue lines

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogueText;
+    public Image avatarImage;
+    public bool dialogueActive;
+
+    public string[] dialogueLines;
+    public int currentDialogLine;
+
+    private void Start()
+    {
+        dialogBox.SetActive(false);
+        dialogueActive = false;
+    }
+
+    private void Update()
+    {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            currentDialogLine++;
+
+            if (currentDialogLine >= dialogueLines.Length)
+            {
+                currentDialogLine = 0;
+                dialogueActive = false;
+                avatarImage.enabled = false;
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogueText.text = dialogueLines[currentDialogLine];
+            }
+        }
+    }
+
+    public void ShowDialogue(string[] lines)
+    {
+        currentDialogLine = 0;
+        dialogueLines = lines;
+        dialogueActive = true;
+        dialogBox.SetActive(true);
+        dialogueText.text = dialogueLines[currentDialogLine];
+    }
+
+    public void ShowDialogue(string[] lines, Sprite sprite)
+    {
+        avatarImage.sprite = sprite;
+        avatarImage.enabled = true;
+        ShowDialogue(lines);
+    }
+}
---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class NPCDialogue : MonoBehaviour
+{
+    public string npcName;
+    public string[] npcDialogueLines;
+    public Sprite npcSprite;
+
+
+    private DialogueManager dialogueManager;
+    private bool playerInTheZone;
+
+    private void Start()
+    {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            playerInTheZone = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            playerInTheZone = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInTheZone && Input.GetMouseButtonDown(1))
+        {
+            string[] finalDialogue = new string[npcDialogueLines.Length];
+            int i = 0;
+            foreach (string line in npcDialogueLines)
+            {
+                finalDialogue[i++] = npcName != null ? $"{npcName}:\n{line}" : line;
+            }
+
+            if (npcSprite)
+            {
+                dialogueManager.ShowDialogue(finalDialogue, npcSprite);
+            }
+            else
+            {
+                dialogueManager.ShowDialogue(finalDialogue);
+            }
+        }
+    }
+}
