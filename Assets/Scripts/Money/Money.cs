9a219b13c0e7ab9bbda71038daee26b77a2ff6d3
Rafa Gonzalez <softlightwebinfo@gmail.com>
2020-04-19 23:23:12 +0200

    Add VirtualGoods

---
+++ Assets/Scripts/Money/Money.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Money : MonoBehaviour
+{
+    public int value;
+
+    private MoneyManager manager;
+
+    private void Start()
+    {
+        manager = FindObjectOfType<MoneyManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            manager.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Money/MoneyManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public int currentMoney;
+    public Text moneyText;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Money"))
+        {
+            currentMoney = PlayerPrefs.GetInt("Money");
+        }
+        else
+        {
+            currentMoney = 0;
+            PlayerPrefs.SetInt("Money", 0);
+        }
+        moneyText.text = currentMoney.ToString();
+    }
+
+    public void AddMoney(int moneyCollected)
+    {
+        currentMoney += moneyCollected;
+        moneyText.text = currentMoney.ToString();
+        PlayerPrefs.SetInt("Money", currentMoney);
+    }
+}
